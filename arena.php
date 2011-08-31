178fafa5df9e8e782303f4212988ddfc7ffac71a
Senthil Padmanabhan <senthil.padmanabhan@yahoo.com>
2011-08-31 15:51:42 -0700

    Updated README

--- arena.php
+++ arena.php
@@ -1,6 +1,6 @@
 <?php 
-	// Vehicle Count
-	$VEH_COUNT = 8;
+	// Picture Count
+	$PIC_COUNT = 8;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <HTML>
@@ -29,7 +29,7 @@
 		</div>
 		<div id="dropBox" class="dropbox">
 			<?php 
-				for($i=0; $i < $VEH_COUNT; $i++) {
+				for($i=0; $i < $PIC_COUNT; $i++) {
 					if($i%4 == 0) {
 						echo '<div class="clear"></div>';
 					}
@@ -44,32 +44,35 @@
 	<script src="picmanager.js"></script>
 	<script>
 		var PicManConfig = {
-			MAX_LIMIT: <?php echo $VEH_COUNT; ?>,
-			uploadForm: "file_upload_form", 
-			file: "picfile", 
-			maskLayer: "mask",
-			addPicLayer: "add",
-			serverURL: "upload.php",
-			errorImage: "no-pic-error.png",
-			dropBox: "dropBox",
-			dropText: "dropText",
-			primaryIndex: 0,
-			startIndex: 0,
-			imageHash: null,
+			MAX_LIMIT: <?php echo $PIC_COUNT; ?>, // Total number of pic placeholders 
+			uploadForm: "file_upload_form", // Form element ID which contains the HTML file element
+			file: "picfile", // HTML file element ID
+			maskLayer: "mask", // The mask div element ID which creates a background mask during zoom and when upload is progressing 
+			addPicLayer: "add", // The div element id which contains the Add Picture link and HTML file element 
+			serverURL: "upload.php", // The AJAX server URL
+			errorImage: "no-pic-error.png", // Error image URL displayed when an error occurs during upload
+			dropBox: "dropBox", // The Div element ID of the drop box for drag & drop
+			dropText: "dropText", // The Div element ID wrapping the text mentioning the feature supports drag & drop
+			imageHash: null, // An array of image info objects, default null but when already uploaded images the object is set
+				 			 // Format: [{thumbnailUrl: "http://xyz", mainUrl: "http://abc"}]
+			primaryIndex: 0, // The index of the image which should be made primary, default 0
+			startIndex: 0, // The start index in the arena, default 0 but when already uploaded images the index is set accordingly
+			// Below attributes are associated with every image and all IDs are indexed from 0
+			// For e.g. the pic container div ID at first location will be picContainer0 and so on
 			image: { 
-				fileNameLayer: "fileName", 
-				progressMeterLayer: "progressMeter",
-				progressLayer: "progress",
-				percentLayer: "percentage", 
-				imageWrapper: "imageWrapper",
-				controlsLayer: "controls",
-				overlayLayer: "overlay",
-				zoomLayer: "enlarge",
-				picContainer: "picContainer",
-				closeZoomLink: "closeZoomLink",
-				zoomControl: "zoom", 
-				primaryControl: "primary",
-				deleteControl: "delete"
+				picContainer: "picContainer", // The container Div element ID of a picture
+				fileNameLayer: "fileName", // The Div element ID in the pic container which holds the file name
+				progressMeterLayer: "progressMeter", // The Div element ID which holds the progress meter elements				
+				progressLayer: "progress", // The inner Div element ID of the progress meter which shows progress
+				percentLayer: "percentage", // The Div element ID showing the progress percentage
+				imageWrapper: "imageWrapper", // The Div element ID wrapping the thumbnail image
+				controlsLayer: "controls", // The Div element ID which contains the dashboard controls
+				overlayLayer: "overlay",// The Div element ID of the overlay for zooming an image
+				zoomLayer: "enlarge", // The Div element ID wrapping the zoom image				
+				closeZoomLink: "closeZoomLink", // The anchor ID for the close link in overlay
+				zoomControl: "zoom", // The Div element ID for the zoom control
+				primaryControl: "primary", // The Div element ID for the make primary image control
+				deleteControl: "delete" // The Div element ID for the delete image control
 			}		
 		};		
 		// Initialize PicMan
